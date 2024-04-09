24304cd531765facac521cdea3a12f2521886841
forax <forax@univ-mlv.fr>
2024-04-09 21:39:49 +0200

    implements enhanced version as parameter

--- src/main/java/com/github/forax/einherjar/core/ValueTypeRewriter.java
+++ src/main/java/com/github/forax/einherjar/core/ValueTypeRewriter.java
@@ -9,16 +9,16 @@ import static org.objectweb.asm.Opcodes.V23;
 public class ValueTypeRewriter extends ClassVisitor {
   private static final int ACC_IDENTITY = ACC_SUPER;
 
-  private final int version;
+  private final int enhancedVersion;
 
-  public ValueTypeRewriter(ClassVisitor classVisitor, int version) {
+  public ValueTypeRewriter(ClassVisitor classVisitor, int enhancedVersion) {
     super(ASM9, classVisitor);
-    this.version = version;
+    this.enhancedVersion = enhancedVersion;
   }
 
   @Override
   public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
     int newAccess = access & ~ACC_IDENTITY;
-    super.visit(V23 | 0xFFFF0000, newAccess, name, signature, superName, interfaces);
+    super.visit((V23 + enhancedVersion - 23) | 0xFFFF0000, newAccess, name, signature, superName, interfaces);
   }
 }
